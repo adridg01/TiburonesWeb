9817a5e4c5fbe3239832a14e5dbd2450f513e1f1
Joel <username@domain.example>
2023-12-21 10:25:38 +0100

    Commit joel Comprobación mascara completa

--- Reto1/src/Recoger.java
+++ Reto1/src/Recoger.java
@@ -93,7 +93,7 @@ public class Recoger {
             }
             if (Integer.parseInt(seleccion) == 1){
                 System.out.println("Dime la mascara");
-
+//
                 do {
                     String MString = scan.nextLine();
                     matcher = pattern.matcher(MString);
