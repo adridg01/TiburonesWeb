374ad14cb09c1b641cf09c6f54c74834c41b79b7
adri1 <adridg01@gmail.com>
2023-12-26 19:43:45 +0100

    commit Adrian conversion a binario

--- Reto1/src/Conversor.java
+++ Reto1/src/Conversor.java
@@ -1,9 +1,36 @@
-import java.util.Scanner;
-
 public class Conversor {
+    public static String[] convertirIpABinario(int[] ip) {
+        String[] binario = new String[4];
+
+        for (int i = 0; i < ip.length; i++) {
+            int parteDecimal = Integer.parseInt(String.valueOf(ip[i]));
+            String parteBinaria = Integer.toBinaryString(parteDecimal);
+
+            // Asegurar que la parte binaria tenga 8 bits
+            while (parteBinaria.length() < 8) {
+                parteBinaria = "0" + parteBinaria;
+            }
+
+            binario[i] = parteBinaria;
+        }
+
+        return binario;
+    }
+
     public static void main(String[] args) {
-        Scanner scn= new Scanner(System.in);
-        String IP=scn.nextLine();
-        IP.toCharArray();
+        System.out.println("Ingrese una dirección IP:");
+        int[] ip = Recoger.ip();
+
+        System.out.println("Dirección IP ingresada: ");
+        for (int parte : ip) {
+            System.out.print(parte + ".");
+        }
+
+        String[] binario = convertirIpABinario(ip);
+
+        System.out.println("\nDirección IP en binario: ");
+        for (String parte : binario) {
+            System.out.print(parte + " ");
+        }
     }
 }
